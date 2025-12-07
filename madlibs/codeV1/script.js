6f529d97a350dadc0563e283ca91e78d16f0b8d6
Hanjie Chen <hjcchen@ucdavis.edu>
2025-12-06 17:36:39 -0800

    madLibs codeV1 complete with CSS

---
+++ madlibs/codeV1/script.js
@@ -0,0 +1,10 @@
+(function (){
+
+    'use strict'
+
+    document.querySelector('#submit').addEventListener('click', function(event){
+        event.preventDefault();
+        document.querySelector('#overlay').className = 'hidden';
+    });
+
+}());
